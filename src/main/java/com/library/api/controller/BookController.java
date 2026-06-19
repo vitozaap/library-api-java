d69e857b3d875bd4a65faadbbce35061e54491e6
Victor Santos <menezes.victorsantos@outlook.com>
2026-06-19 13:19:17 -0300

    feat: finished book endpoints

--- src/main/java/com/library/api/controller/BookController.java
+++ src/main/java/com/library/api/controller/BookController.java
@@ -1,11 +1,20 @@
 package com.library.api.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.library.api.model.Book;
 import com.library.api.service.BookService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
@@ -21,11 +30,26 @@ public class BookController {
         return service.getALl();
     }
 
+    @GetMapping("/{id}")
+    public Book getById(@PathVariable String id) {
+        return service.findById(id);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book) {
-
         return service.create(book);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public Book update(@RequestBody Book book, @PathVariable String id) {
+        return service.update(id, book);
     }
 
 }
